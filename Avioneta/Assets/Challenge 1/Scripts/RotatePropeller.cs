38e37513b4395437063689e20fb35f0340e1cadf
Dolores Ruiz <mdrg0010@red.ujaen.es>
2020-10-15 17:28:31 +0200

    Ejercicio finalizado

---
+++ Avioneta/Assets/Challenge 1/Scripts/RotatePropeller.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatePropeller : MonoBehaviour
+{
+    public GameObject plane;
+    private float speed = 360*10;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.Rotate(Vector3.forward, speed * Time.deltaTime);
+    }
+}
